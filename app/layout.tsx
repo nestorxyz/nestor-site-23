edd3fadf509e3235c5363ae5859b66671f7fade8
Nestor Mamani <nmamanipantoja@gmail.com>
2023-02-03 22:32:12 -0500

    vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import '@/styles/globals.css';
 import { Heebo } from '@next/font/google';
+import { AnalyticsWrapper } from '@/components/Analytics';
 
 const heebo = Heebo({
   subsets: ['latin'],
@@ -16,6 +17,7 @@ export default function RootLayout({
     <html lang="en" className={heebo.variable}>
       <body className="font-heebo bg-black mx-auto max-w-4xl px-4 lg:px-0 py-5 md:py-10 flex flex-col gap-10">
         {children}
+        <AnalyticsWrapper />
       </body>
     </html>
   );
